f3b8b9585e0b81b386b4443f9467f70c12144087
Yonas Kenenisa <48440474+yonasTK@users.noreply.github.com>
2022-09-26 10:52:33 +0300

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
+
 def divisible_by_2(my_list=[]):
     if my_list:
-        values = []
-        for i in my_list:
-            if i % 2 is 0:
-                values.append(True)
-            else:
-                values.append(False)
-        return values
-
-# return [(i % 2 == 0) for i in my_list]
+        return list(map(lambda n: not (n % 2), my_list))
+    return None
