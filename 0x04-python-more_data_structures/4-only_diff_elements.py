ead4774f8a0ee694c0e0650c53aaaeeafdfbf631
Yonas Kenenisa <48440474+yonasTK@users.noreply.github.com>
2022-09-28 16:33:30 +0300

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
 def only_diff_elements(set_1, set_2):
-        return(set_1 ^ set_2)
+    """ Print the elements unique to each set (i.e. disjoint union)
+    """
+    if set_1 is not None and set_2 is not None:
+        return set(set_1) ^ set(set_2)
+    return None
