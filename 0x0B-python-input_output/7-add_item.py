f6879573ea4d2a95d24be127e671b409723fede3
Yonas Kenenisa <48440474+yonasTK@users.noreply.github.com>
2022-10-26 11:06:15 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-"""
-Module 9-add_item
-Contains function that adds and saves to Python obj to JSON file; loads objects
-# run with ./9-add_item.py
-#
-# cat add_item.json ; echo ""
-# expect output: []
-#
-# ./9-add_item.py some random args
-# cat add_item.json ; echo ""
-# expect output: ["some", "random", "args"]
-"""
+'''task 7 module'''
 
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-filename = "add_item.json"
+arglist = list(sys.argv[1:])
 
 try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_content = []
+    old_data = load_from_json_file('add_item.json')
+except Exception:
+    old_data = []
 
-save_to_json_file(existing_content + argv[1:], filename)
+old_data.extend(arglist)
+save_to_json_file(old_data, 'add_item.json')
