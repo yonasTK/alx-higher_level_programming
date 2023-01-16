5877367fff7ae6e7a6df2072c79aebdb1636e03d
Yonas Kenenisa <48440474+yonasTK@users.noreply.github.com>
2023-01-16 16:05:19 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,19 +4,20 @@ Provides a function to find a peak element in an unsorted list of integers
 """
 
 
-def find_peak(list_of_integers):
+
+def find_peak(integers):
     """
     Finds a peak element in an unsorted list of integers
     """
-    if not list_of_integers:
+    if not integers:
         return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    if len(list_of_integers) == 2:
-        return list_of_integers[0] if list_of_integers[0] > list_of_integers[1] else list_of_integers[1]
-    midpoint = len(list_of_integers) // 2
-    if list_of_integers[midpoint] < list_of_integers[midpoint - 1]:
-        return find_peak(list_of_integers[:midpoint])
-    if list_of_integers[midpoint] < list_of_integers[midpoint + 1]:
-        return find_peak(list_of_integers[midpoint + 1:])
-    return list_of_integers[midpoint]
+    if len(integers) == 1:
+        return integers[0]
+    if len(integers) == 2:
+        return integers[0] if integers[0] > integers[1] else integers[1]
+    midpoint = len(integers) // 2
+    if integers[midpoint] < integers[midpoint - 1]:
+        return find_peak(integers[:midpoint])
+    if integers[midpoint] < integers[midpoint + 1]:
+        return find_peak(integers[midpoint + 1:])
+    return integers[midpoint]
